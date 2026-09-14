1c1d64b76f024274b11d99e34dad35e13e38ac07
hajar elad <hajar.elad@gmail.com>
2026-09-14 21:30:31 +0200

    removing extra pages

--- home/home-scripts/index.js
+++ home/home-scripts/index.js
@@ -1,22 +1,10 @@
 import { header } from "../../shared-scripts/header.js";
-import { homeTopSectionAdvantages } from "./homeTopSection.js";
-import { homeAboutSection } from "./homeMidSection.js";
-import { homePlansSection } from "./homeMidSection.js";
 import { footer } from "../../shared-scripts/footer.js";
 
 const logoIcon = './images/icons/logoIcon.png';
  
 document.querySelector('.header')
     .innerHTML = header('./');
-
-document.querySelector('.home-advantages-section')
-    .innerHTML = homeTopSectionAdvantages();
-
-document.querySelector('.home-about-section')
-    .innerHTML = homeAboutSection();
-
-document.querySelector('.home-plans-section')
-    .innerHTML = homePlansSection();
 
 document.querySelector('.footer')
     .innerHTML = footer();
